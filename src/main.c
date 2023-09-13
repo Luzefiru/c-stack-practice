0e76ff06da422392b0f0c02b6e772ad95ecdd3c8
Luzefiru <ian.deejay.iv@gmail.com>
2023-09-13 11:16:20 +0800

    fix: resolve error

--- src/main.c
+++ src/main.c
@@ -5,9 +5,6 @@ int main(void) {
   puts("\nTesting a function defined in /src/stack/$(IMPLEMENTATION)/stack.c, included via stack.h");
   debugPrint();
 
-  Stack pog = init();
-  pop(pog);
-
   puts("Press Any Key to Continue...");  
   getchar();    
 }
