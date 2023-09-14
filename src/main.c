c143445f341b22d6c0fa55d0cde3e91cd6fe5139
Reece Lim <lim.reecesergei@gmail.com>
2023-09-14 13:26:12 +0800

    nit: add colors to print

--- src/main.c
+++ src/main.c
@@ -17,58 +17,84 @@ int main(void)
   debugPrint();
 
   // /* test program */
+  printf("\e[0;33m");
   puts("Stack s = init()");
+  printf("\e[0m");
   Stack s = init();
   puts("");
 
+  printf("\e[0;32m");
   puts("top(s)");
+  printf("\e[0m");
   printf("> the element at the top of the stack = %c (integer value = %d)\n", top(s), top(s));
   puts("");
 
+  printf("\e[0;31m");
   puts("isEmpty(s)");
+  printf("\e[0m");
   isEmpty(s) ? puts("> true") : puts("> false");
   puts("");
 
+  printf("\e[0;31m");
   puts("isFull(s)");
+  printf("\e[0m");
   isFull(s) ? puts("> true") : puts("> false");
   puts("");
 
+  printf("\e[0;36m");
   puts("push(s, 'U')");
+  printf("\e[0m");
   push(s, 'U');
   printf("> new length of stack = %d\n", length(s));
   puts("");
 
+  printf("\e[0;32m");
   puts("top(s)");
+  printf("\e[0m");
   printf("> the element at the top of the stack = %c (integer value = %d)\n", top(s), top(s));
   puts("");
 
+  printf("\e[0;36m");
   puts("push(s, 'A')");
+  printf("\e[0m");
   push(s, 'A');
   printf("> new length of stack = %d\n", length(s));
   puts("");
 
+  printf("\e[0;34m");
   puts("displayStack(s)");
+  printf("\e[0m");
   printf("> ");
   displayStack(s);
   puts("");
 
+  printf("\e[0;32m");
   puts("top(s)");
+  printf("\e[0m");
   printf("> the element at the top of the stack = %c (integer value = %d)\n", top(s), top(s));
   puts("");
 
+  printf("\e[0;35m");
   puts("pop(s)");
+  printf("\e[0m");
   pop(s);
   puts("");
 
+  printf("\e[0;32m");
   puts("top(s)");
+  printf("\e[0m");
   printf("> the element at the top of the stack = %c (integer value = %d)\n", top(s), top(s));
   puts("");
 
+  printf("\e[0;34m");
   puts("displayStack(s)");
+  printf("\e[0m");
   displayStack(s);
   puts("");
 
+  printf("\e[0;32m");
   puts("pushing: R I F E Z");
+  printf("\e[0m");
   push(s, 'R');
   push(s, 'I');
   push(s, 'F');
@@ -77,63 +103,84 @@ int main(void)
   printf("> new length of stack = %d\n", length(s));
   puts("");
 
+  printf("\e[0;34m");
   puts("displayStack(s)");
+  printf("\e[0m");
   printf("> ");
   displayStack(s);
   puts("");
 
+  printf("\e[0;31m");
   puts("isFull(s)");
+  printf("\e[0m");
   isFull(s) ? puts("> true") : puts("> false");
   puts("");
 
+  printf("\e[0;36m");
   puts("push(s, 'X')");
+  printf("\e[0m");
+  push(s, 'X');
+  
+  printf("\e[0;36m");
+  puts("push(s, 'X')");
+  printf("\e[0m");
+  push(s, 'X');
+
+  printf("\e[0;36m");
+  puts("push(s, 'X')");
+  printf("\e[0m");
+  push(s, 'X');
+
+  printf("\e[0;36m");
+  puts("push(s, 'X')");
+  printf("\e[0m");
+  push(s, 'X');
+
+  printf("\e[0;36m");
+  puts("push(s, 'X')");
+  printf("\e[0m");
+  push(s, 'X');
+
+  printf("\e[0;36m");
+  puts("push(s, 'X')");
+  printf("\e[0m");
   push(s, 'X');
   puts("");
 
-  puts("push(s, 'X')");
-  push(s, 'X');
-  puts("");
-
-  puts("push(s, 'X')");
-  push(s, 'X');
-  puts("");
-
-  puts("push(s, 'X')");
-  push(s, 'X');
-  puts("");
-
-  puts("push(s, 'X')");
-  push(s, 'X');
-  puts("");
-
-  puts("push(s, 'X')");
-  push(s, 'X');
-  puts("");
-
+  printf("\e[0;34m");
   puts("displayStack(s)");
+  printf("\e[0m");
   printf("> ");
   displayStack(s);
   printf("> new length of stack = %d\n", length(s));
   puts("");
 
+  printf("\e[0;31m");
   puts("isFull(s)");
+  printf("\e[0m");
   isFull(s) ? puts("> true") : puts("> false");
   puts("");
 
   puts("Popping all elements...\n");
   while (!isEmpty(s))
   {
+    printf("\e[0;35m");
     puts("pop(s)");
+    printf("\e[0m");
     pop(s);
-    puts("");
   }
+  puts("");
 
+  printf("\e[0;34m");
   puts("displayStack(s)");
+  printf("\e[0m");
   printf("> ");
   displayStack(s);
   puts("");
 
+  printf("\e[0;31m");
   puts("isEmpty(s)");
+  printf("\e[0m");
   isEmpty(s) ? puts("> true") : puts("> false");
   printf("> new length of stack = %d\n", length(s));
   puts("");
